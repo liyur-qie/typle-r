c231ef0fb265e2dea4697d97403fa27489872580
liyur-qie <liyur@outlook.jp>
2024-05-25 23:45:59 +0900

    Fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Sidebar from "@/components/Sidebar/Sidebar";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex">
       <Sidebar />
       <main>
         <p>Content</p>
       </main>
-    </>
+    </div>
   );
 }
