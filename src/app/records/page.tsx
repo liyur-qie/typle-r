831b8eac480d680573d266e38b984ed94bae7600
liyur-qie <liyur@outlook.jp>
2024-09-23 08:19:10 +0900

    /recordsのtableをmuiに移行

--- src/app/records/page.tsx
+++ src/app/records/page.tsx
@@ -1,38 +1,62 @@
+"use client"
+
 import PageTitle from "@/components/PageTitle/PageTitle"
 import PageDescription from "@/components/PageDescription/PageDescription"
+import WordListsResponse from "@/json/WordListsResponse.json"
+import { useEffect, useState } from "react"
+import { WordList } from "@/types/WordList"
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Button from "@mui/material/Button"
 
 export default function Records() {
+  const [wordLists, setWordLists] = useState<WordList[]>()
+
+  useEffect(() => {
+    setWordLists(WordListsResponse)
+  }, [])
+
   return (
     <main className="bg-white p-16 pb-12">
       <PageTitle>記録</PageTitle>
       <PageDescription>各単語リストの記録を確認することができます。</PageDescription>
-      <section className="mt-12">
-        <h2 className="text-2xl">単語集A</h2>
-        <table className="mt-4">
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>所要時間</th>
-              <th>単語数</th>
-              <th>練習日時</th>
-              <th>操作</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="text-center">
-                1
-              </td>
-              <td>2秒</td>
-              <td>N 単語</td>
-              <td>2024/09/22 10:00</td>
-              <td>
-                <button>削除</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
+      { wordLists?.map(wordList => (
+        <section className="mt-12" key={ wordList.name }>
+          <h2 className="text-2xl">{ wordList.name }</h2>
+          <TableContainer className="mt-4">
+                <Table aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>No.</TableCell>
+                      <TableCell>所要時間</TableCell>
+                      <TableCell>単語数</TableCell>
+                      <TableCell>練習日時</TableCell>
+                      <TableCell>操作</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    { wordList.records.map((record, index) => (
+                      <TableRow key={ index }>
+                        <TableCell component="th" scope="row">{ index + 1 }</TableCell>
+                        <TableCell> { record.time } 秒</TableCell>
+                        <TableCell>{ wordList.words.length }</TableCell>
+                        <TableCell> { record.time } </TableCell>
+                        <TableCell>
+                          <Button variant="outlined">削除</Button>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+        </section>
+        ))
+      }
     </main>
   )
 }
